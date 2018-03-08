4d5f9f33d2adb6e494e26171d6483438a59672af
WingofTech <r2tao@UCSD.EDU>
2018-03-07 21:22:11 -0800

    "Changed physics settings to display CurvedUI on top of everything else. Also worked on adding the user's shields status on the UI. Also added materials: Steamed, Moon, and Chrome."

---
+++ Assets/Scripts/ChangeUIShader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeUIShader : MonoBehaviour {
+
+  public Shader newShader;
+
+	// Use this for initialization
+	void Start () {
+    Canvas.GetDefaultCanvasMaterial().shader = newShader;
+  }
+}
