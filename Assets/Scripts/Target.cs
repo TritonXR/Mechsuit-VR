e601c6a3b050cb6c53d353a4989b83f3462bdd8f
WingofTech <r2tao@UCSD.EDU>
2018-05-02 20:08:32 -0700

    Fixed railgun shooting bug, added target practice

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  void OnCollisionEnter(Collision collision) {
+    Debug.Log("Triggered OnCollisionEnter");
+    if (collision.gameObject.tag == "Projectile") {
+      //Destroy(this.gameObject);
+      Debug.Log("Ping!");
+    }
+  }
+}
