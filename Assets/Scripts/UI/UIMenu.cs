0d9c4b53c5045d011b587e995eafdace3c6501bd
Yue Wu <yuw264@UCSD.EDU>
2018-05-26 20:25:59 -0700

    Completed basic CurvedUI pause menu

--- Assets/Scripts/UI/UIMenu.cs
+++ Assets/Scripts/UI/UIMenu.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class UIMenu : MonoBehaviour {
 
+  public ArmController leftController, rightController;
+
   public void QuitGame() {
     Debug.Log("Game quitting...");
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
     Application.Quit();
+#endif
   }
 
   public void ReCalibrate() {
     Debug.Log("Restarting calibration...");
-    ArmController.isCalibrated = false;
+    leftController.Reset();
+    rightController.Reset();
   }
 
   public void Resume() {
     Debug.Log("Resuming...");
-    GameObject menu = GameObject.Find("CurvedCanvas");
+    GameObject menu = this.gameObject.transform.parent.gameObject;
     menu.SetActive(false);
   }
 }
