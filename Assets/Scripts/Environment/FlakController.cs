032933ff23fc7743d4af8567100a967e2ab10335
Nicholas <chap.online101@gmail.com>
2018-05-26 20:32:35 -0700

    Tweaking animations and animator. Still broken, rings won't spin on separate layers and blades/grates are closing all at once.

---
+++ Assets/Scripts/Environment/FlakController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlakController : MonoBehaviour {
+
+  Animator anim;
+  //int centerBlades = Animator.StringToHash("FlakCenter");
+  //int middleBlades = Animator.StringToHash("FlakMiddle");
+  //int outerBlades = Animator.StringToHash("FlakOuter");
+
+  Animation centerBlades;
+  Animation middleBlades;
+  Animation outerBlades;
+
+  // Use this for initialization
+  void Start () {
+    //anim = GetComponent<Animator>();
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    /*Animation.Play(centerBlades);
+    Animation.Play(middleBlades);
+    Animation.Play(outerBlades);
+    anim.SetTrigger(centerBlades);
+    anim.SetTrigger(middleBlades);
+    anim.SetTrigger(outerBlades);*/
+  }
+
+  void ActivateBlades() {
+    //this.animation();
+  }
+}
