3ab75d147f1dc33b9edc3fb50195fd7ef4b0f17b
WingofTech <chap.online101@gmail.com>
2019-05-15 21:27:27 -0700

    Added Steamworks.NET with 480 as app ID in SteamManager.cs and steam_appid.txt

---
+++ Assets/Scripts/DisplayName.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class DisplayName : MonoBehaviour
+{
+
+    // Don't use any Steam functions inside Awake as SteamManager is initialized in Awake
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!SteamManager.Initialized) {
+            Debug.Log("SteamManager not initialized");
+            return;
+        }
+
+        Debug.Log("SteamManager successfully initialized");
+        Debug.Log(SteamFriends.GetPersonaName());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
