0e785891f83a8ceef031b99f03c4bae656faadf2
WingofTech <chap.online101@gmail.com>
2019-05-18 13:02:55 -0700

    Worked in TESTING scene and enabled MARUI_GR to the point where it can record, train, recognize, and store gesture data to two separate controller data files.

--- Assets/Scripts/MachineLearning/MURUI Custom/MURUI_GR.cs
+++ Assets/Scripts/MachineLearning/MURUI Custom/MURUI_GR.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using Valve.VR;
 
@@ -10,19 +11,39 @@ public class MURUI_GR : MonoBehaviour {
 
     private GestureRecognition gr;
 
-    private int swingMove;
-    private int summonRailgun;
-    private int summonWhip;
 
+    public bool isLeft;
 
+    public string FileName {
+        get {
+            return isLeft ?
+                "D:/MechSuit-VR/MachineLearning/Murui_Data/MSVR_Gestures_Left.dat" :
+                "D:/MechSuit-VR/MachineLearning/Murui_Data/MSVR_Gestures_Right.dat";
+        }
+    }
+
+    List<int> gestures;
+    int currIndex;
     // Start is called before the first frame update
     void Start() {
         gr = new GestureRecognition();
-        gr.loadFromFile("D:/MechSuit-VR/MachineLearning/Murui_Data/MSVR_Gestures.dat");
-        swingMove = gr.createGesture("Swing move");
-        summonRailgun = gr.createGesture("Summon railgun");
-        summonWhip = gr.createGesture("Summon whip");
-
+        if (File.Exists(FileName)) {
+            gr.loadFromFile(FileName);
+            gestures = new List<int>() {
+                0, 1, 2, 3, 4, 5
+            };
+            print("Loaded from file");
+        } else {
+            gestures = new List<int>() {
+                gr.createGesture("Swing move"),
+                gr.createGesture("Summon sword"),
+                gr.createGesture("Summon bow"),
+                gr.createGesture("Summon railgun"),
+                gr.createGesture("Summon whip"),
+                gr.createGesture("Summon gauntlets")
+            };
+            print("Created new file");
+        }
     }
 
     private bool isRecording;
@@ -30,15 +51,26 @@ public class MURUI_GR : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
         if (!isRecognizing) {
             //Record Data
-            Record(swingMove);
+            Record(gestures[currIndex]);
         } else {
             //Recognize Data
             int result = Record();
-            if (result == swingMove) {
-                Debug.Log("This gesture is: swingMove");
+
+            if (result != -2) {
+                bool found = false;
+                foreach (int gesture in gestures) {
+                    if (result == gesture) {
+                        print(string.Format("Gesture is {0}", gr.getGestureName(gesture)));
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    print("Not found");
+                }
             }
         }
 
@@ -48,19 +80,15 @@ public class MURUI_GR : MonoBehaviour {
         }
 
         if (Input.GetKeyDown(KeyCode.Y)) {
-            if (!isRecognizing) 
-            {
-                isRecognizing = true;
-                Debug.Log("True.");
-            }
-
-            else {
-                isRecognizing = false;
-                Debug.Log("False.");
-            }
+            SwitchMode();
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="gesture"></param>
+    /// <returns>-2 if in the process of recording.</returns>
     int Record(int gesture = -1) {
         if (triggerPulled.GetState(trackedObj.inputSource)) {
             if (!isRecording) {
@@ -75,25 +103,32 @@ public class MURUI_GR : MonoBehaviour {
                 Quaternion q = trackedObj.transform.localRotation;
                 gr.contdStroke(p, q);
                 // ^ repeat while performing the gesture with your controller.
-                Debug.Log("Still recording.");
             }
         } else if (isRecording) {
             isRecording = false;
             Debug.Log("Done recording!!");
             return gr.endStroke();
-
         }
 
-        return -1;
+        return -2;
     }
 
-    void Train() {
+    public void Train() {
         gr.setMaxTrainingTime(10000); // Set training time to 10 seconds.
         gr.startTraining();
         print("Training done");
     }
 
+    public void SwitchMode() {
+        isRecognizing = !isRecognizing;
+        print(isRecognizing ? "Recognization mode" : "Training mode");
+    }
+
+    public void SwitchGesture(int index) {
+        currIndex = index;
+    }
+
     private void OnApplicationQuit() {
-        gr.saveToFile("D:/MechSuit-VR/MachineLearning/Murui_Data/MSVR_Gestures.dat");
+        gr.saveToFile(FileName);
     }
 }
